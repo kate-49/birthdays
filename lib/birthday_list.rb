8deb33c29beb22d40d6e66ec110f9790e66ddefb
kate smith <kateesmith4994@gmail.com>
2020-05-13 09:26:02 +0100

    completed exercise

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,45 @@
+require 'date'
 
+class BirthdayList
+attr_accessor :today
+
+  def initialize
+    @today = (Time.now).to_date
+    @birthdays = []
+    @birthdays_today = {}
+    @birthdays << {"Person one" => Date.parse('1990-05-13')}
+    @birthdays << { "Bob Bobcat" => Date.parse('15-01-1999')}
+    @birthdays << { "Generic Person" => Date.parse('01-01-2001')}
+    @birthdays << { "New Person" => Date.parse('13-05-2010')}
+  end
+  
+  def print_all_birthdays
+    ans = []
+    @birthdays.each {|hash|
+      hash.each { |name, date|
+        ans << "#{name} was born on #{date}"
+      }
+    }
+    return ans
+  end
+
+  def birthday_is_today
+    @birthdays.each {|hash|
+      hash.each do |name, date|
+        if (date.day == today.day) && (date.month == today.month)
+          @birthdays_today[name] = (today.year - date.year)
+        end
+      end
+    }
+     array = @birthdays_today.keys
+     array.join(", ")
+  end
+
+  def print_todays_birthdays
+    ans = []
+    @birthdays_today.each {|name, age|
+      ans << "#{name} has their birthday today, they are #{age} years old!"
+  }
+    ans.join(" ")
+  end
+ end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,20 @@
+require './lib/birthday_list.rb'
 
+describe BirthdayList do
+
+  it 'can return birthdays stored in hash' do
+    expect(subject.print_all_birthdays).to eq ["Person one was born on 1990-05-13",
+      "Bob Bobcat was born on 1999-01-15", "Generic Person was born on 2001-01-01",
+       "New Person was born on 2010-05-13"]
+  end
+
+  it 'can return names of people whos birthday is today' do
+    expect(subject.birthday_is_today).to eq "Person one, New Person"
+  end
+
+  it 'can nicely print birthdays and ages' do
+    subject.birthday_is_today
+    expect(subject.print_todays_birthdays).to eq("Person one has their birthday today, they are 30 years old! New Person has their birthday today, they are 10 years old!")
+  end
+
+end
